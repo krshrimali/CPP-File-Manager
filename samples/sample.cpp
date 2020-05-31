abfaace703409495409105f1215768c1e8332592
krshrimali <kushashwaravishrimali@gmail.com>
2020-05-31 11:24:10 +0530

    Update sample code

--- samples/sample.cpp
+++ samples/sample.cpp
@@ -2,12 +2,7 @@
 #include "FileManager.hpp"
 
 int main(int argc, char** argv) {
-    FileManager file(argv[1]);
-    file.info();
-    /* file.list_files_new() ;
-    for (auto const& i: file.list_files_new()) {
-        std::cout << i.name << std::endl;
-        std::cout << "is_dir: " << i.is_dir << std::endl ;
-    } */
-    file.writeToFile(file, /*ignore_dirs=*/ {"build", ".git"});
+  FileManager file(argv[1]);
+  file.info();
+  file.writeToFile(file, /*ignore_dirs=*/ {"build", ".git", ".github", ".vscode"});
 }
