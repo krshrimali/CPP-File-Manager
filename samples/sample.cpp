9c569fdfc7efa019bcf79b5058d8d805ad14f862
krshrimali <kushashwaravishrimali@gmail.com>
2020-06-02 07:54:49 +0530

    Update sample code

--- samples/sample.cpp
+++ samples/sample.cpp
@@ -4,5 +4,9 @@
 int main(int argc, char** argv) {
   FileManager file(argv[1]);
   file.info();
+  auto out = file.list_files({".txt"});
+  for(auto const& i: out) {
+    std::cout << i.name << ", " << i.name << std::endl;
+  }
   file.writeToFile(file, /*ignore_dirs=*/ {"build", ".git", ".github", ".vscode"});
 }
