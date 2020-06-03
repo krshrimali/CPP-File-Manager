f1051c8cfbbe6242032e0cd80f386dea27d6e05b
krshrimali <kushashwaravishrimali@gmail.com>
2020-06-03 10:49:47 +0530

    Update library and header file

--- samples/FileManager.hpp
+++ samples/FileManager.hpp
@@ -20,19 +20,17 @@ class FileManager {
 
     FileManager(std::string path) {
       // The whole class will potentially revolves around the subfolders inside the path (including the path)
-      corePath = path;
+      this->corePath = path;
     }
   
     void clear();
     void clear(std::string);
     void info();
     std::vector<file_info> list_files(std::vector<std::string>, bool);
-    // TODO: Why to pass FileManager object?
-    void writeToFile(FileManager, std::vector<std::string>, std::vector<std::string>);
+    void writeToFile(std::vector<std::string>, std::vector<std::string>);
   private:
     std::string corePath;
-    // TODO: Why to pass FileManager object, and pass everything by reference!
-    void writeToFileIterated(FileManager, std::ofstream&, int, std::vector<std::string>, std::vector<std::string>);
+    void writeToFileIterated(std::ofstream&, int, std::vector<std::string>, std::vector<std::string>);
     bool itemInList(std::string, std::vector<std::string>);
     file_info make_file_info(std::string, std::string, bool);
 };
